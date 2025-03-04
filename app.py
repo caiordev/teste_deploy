80e6dffc7b1d0b3b1072af5ed28168dbee79ba2b
caiordev <caioreisdev@gmail.com>
2025-03-03 23:49:10 -0300

    five commit

--- app.py
+++ app.py
@@ -107,4 +107,6 @@ def monitor():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+#ssl_context=('cert.pem', 'key.pem')
